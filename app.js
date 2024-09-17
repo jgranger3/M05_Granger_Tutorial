f857c31de63ab538d50084fc959ba4e6f9d21aa8
jgranger3 <jgranger3@ivytech.edu>
2024-09-17 12:44:26 -0400

    learning about ejs

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+//express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+// listen for request
+app.listen(3000);
+
+app.get('/', (req, res) =>{
+    //res.send('<p>home page</p>');
+    res.render('index');
+});
+
+app.get('/about', (req, res) =>{
+    //res.send('<p>about page</p>');
+    res.render('about');
+});
+
+//redirects
+app.get('/about-us', (req, res) =>{
+    res.redirect('/about');
+});
+
+// 404 page
+app.use((req, res) =>{
+    res.status(404).render('404');
+});
